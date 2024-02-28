9154044cada6b23eb653c70360f905c0a695d871
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2024-02-28 14:04:04 +0100

    husky testing

---
+++ src/server.ts
@@ -0,0 +1,3 @@
+const abra = 'abracadabra'
+
+console.log(abra)
---
+++ test/routes/example.test.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import assert from 'node:assert'
+import { describe, it } from 'node:test'
+
+describe('should run the script', () => {
+    it('scripting test', () => {
+        console.log('run')
+
+        assert(true)
+    })
+})
